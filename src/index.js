d59d0bd80f30c64c98212a37cec9e3768fe65805
gusap9 <nikitaasauleak@gmail.com>
2022-01-09 14:57:41 +0300

    final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let i = 0;
+    let result = 0;
+    while (n) {
+        i = n % 10;
+        result = (result * 10) + i;
+        n = n/10|0;
+    };
+    return Math.abs(result);
 }
